c2aafc6cf03cb5a08299e27d01b860a2a253f393
trevorw1990 <95665741+trevorw1990@users.noreply.github.com>
2022-04-08 22:28:15 -0400

    Update App.js

--- jeopardy-app/src/App.js
+++ jeopardy-app/src/App.js
@@ -1,2 +1,30 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+
+function App() {
+    const [data, setData] = useState(0);
+    const [Message, setMessage] = useState({
+      content: "",
+      type: "",
+    });
+    const [score, setScore] = useState(0);
+  
+    const getData = async () => {
+      const response = await fetch("https://jservice.io/api/random");
+      const data = await response.json();
+      setData(data);
+      setMessage({
+        ...Message,
+        ["content"]: data[0].question,
+        ["type"]: "Question",
+      });
+    };
+    // useEffect(() => {
+    //   getData();
+    // }, []);
+  
+    const handleClick = async (e) => {
+      getData();
+    };
+
+    
