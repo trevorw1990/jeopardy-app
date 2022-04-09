7aa6aa6f52b803704813276725699ec48e37f6ed
trevorw1990 <95665741+trevorw1990@users.noreply.github.com>
2022-04-08 22:46:20 -0400

    complete update

--- jeopardy-app/src/App.js
+++ jeopardy-app/src/App.js
@@ -19,12 +19,108 @@ function App() {
         ["type"]: "Question",
       });
     };
-    // useEffect(() => {
-    //   getData();
-    // }, []);
+
   
     const handleClick = async (e) => {
       getData();
     };
 
+    const Reveal = (e) => {
+        setMessage({
+          ...Message,
+          ["content"]: data[0].answer,
+          ["type"]: "Answer",
+        });
+      };
+    
+      const RevealQuestion = (e) => {
+        setMessage({
+          ...Message,
+          ["content"]: data[0].question,
+          ["type"]: "Question",
+        });
+      };
+    
+ 
+    
+    
+      const Decrease = () => {
+        setScore(score - data[0].value);
+      };
+      const Increase = () => {
+        setScore(score + data[0].value);
+      };
+      const Rest = () => {
+        setScore(0);
+      };
+      return (
+        <div className="App">
+          <div className="main">
+            <h1 className="title">Welcome to Jeopardy!</h1>
+            <h1 className="score">
+              Score : <span style={{ color: "white" }}>{score}</span>
+            </h1>
+            <button onClick={Decrease} className="decrease">
+              Decrease
+            </button>
+            <button onClick={Increase} className="increase">
+              Increase
+            </button>
+            <button onClick={Rest} className="rest">
+              Reset
+            </button>
+            <br />
+            <h1 className="score"> Let's play !</h1>
+    
+     
+            <button onClick={handleClick} className="get-question">
+              Get Question
+            </button>
+    
+            <h1 className="score">
+              Category :
+              <span style={{ color: "white" }}>
+                {data == 0 ? <h1></h1> : data[0].category.title}
+              </span>
+            </h1>
+            <h1 className="score">
+              Points :
+              <span style={{ color: "white" }}>
+                {data == 0 ? <h1></h1> : data[0].value}
+              </span>
+            </h1>
+    
+            {Message["content"] == "" ? (
+              <h1>click Get Question to display Question</h1>
+            ) : (
+              <h1>
+                <span style={{ color: "yellow" }}> {Message["type"]} :</span>
+                {Message["content"]}
+              </h1>
+            )}
+    
+            {Message["type"] == "Question" ? (
+              <button onClick={Reveal} className="reveal">
+                click to reveal the answer
+              </button>
+            ) : (
+              <button onClick={RevealQuestion} className="reveal">
+                click to reveal the Question
+              </button>
+            )}
+    
+            <button
+              onClick={Reveal}
+              className="reveal"
+              name="question"
+              style={{ display: "none" }}
+            >
+              click to reveal the answer
+            </button>
+          </div>
+        </div>
+      );
+    }
+    
+    export default App;
     
